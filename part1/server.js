ac15896be61cc557b572968ae5a51297895f9702
rtungol <rarrtungol@gmail.com>
2023-02-22 22:16:37 -0500

    server-side styling

--- part1/server.js
+++ part1/server.js
@@ -1,12 +1,11 @@
 import express from "express";
 import React from "react";
+import { ServerStyleSheet } from "styled-components";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import path from "path";
 import fs from "fs";
 import App from "./src/App";
-
-
 
 const app = express();
 
@@ -14,10 +13,14 @@ const app = express();
 app.use(express.static("./build", { index: false }));
 
 app.get("/*", (req, res) => {
+  const sheet = new ServerStyleSheet();
+
   const reactApp = renderToString(
-    <StaticRouter location={req.url}>
-      <App />
-    </StaticRouter>
+    sheet.collectStyles(
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>
+    )
   );
   const templateFile = path.resolve("./build/index.html");
   fs.readFile(templateFile, "utf8", (err, data) => {
@@ -25,7 +28,9 @@ app.get("/*", (req, res) => {
       return res.status(500).send(err);
     }
     return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+      data
+        .replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+        .replace("{{styles}}", sheet.getStyleTags())
     );
   });
 });
--- part1/src/App.js
+++ part1/src/App.js
@@ -3,11 +3,17 @@ import { Switch, Route, Link } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Articles } from "./pages/Articles";
+import styled from "styled-components";
+const BigGreenHeading = styled.h1`
+  color: green;
+  font-size: 96px;
+  `
+
 
 const App = () => {
   return (
     <>
-      <h1>Server-Side Rendering Example</h1>
+      <BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
       <ul>
         <li>
           <Link to="/">Home</Link>
